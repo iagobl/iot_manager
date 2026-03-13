a6b2801bc48fa68116bd4f8ef4b749a7ef6f589b
iago <iago.blopez@udc.es>
2026-03-13 12:29:58 +0100

    chore: initial flutter project structure

---
+++ lib/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const supabaseUrl = 'https://eumoizstcabjryqzqvvg.supabase.co';
+  static const supabaseAnonKey = 'sb_publishable_YoFdr20r7CXPw8LYKGaQPA_r6pNcPt2';
+}
